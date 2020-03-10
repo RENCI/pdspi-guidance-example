680fcece0cbabe3fed7ba0994ad2b269be677eb6
hyi <hongyi@renci.org>
2020-03-10 11:46:03 -0400

    minor modification to utils.py

--- api/utils.py
+++ api/utils.py
@@ -1,7 +1,7 @@
 from random import seed, random
 
 
-def get_random(min_num, max_num):
+def _get_random(min_num, max_num):
     return random() * (max_num - min_num) + min_num
 
 
@@ -10,9 +10,9 @@ def generate_time_series_data(n):
     seed(1)
     for i in range(n):
         if i == 0:
-            y = get_random(0, 1)
+            y = _get_random(0, 1)
         else:
-            y = max(data[i-1]['y'] + get_random(-1, 1), 0)
+            y = max(data[i-1]['y'] + _get_random(-1, 1), 0)
         data.append({
             'x': i,
             'y': y
@@ -28,9 +28,9 @@ def generate_multi_time_series_data(n, m):
         group = "group " + i
         for j in range(n):
             if j == 0:
-                y = get_random(0, 1)
+                y = _get_random(0, 1)
             else:
-                y = max(data[len(data) - 1]['y'] + get_random(-1, 1), 0)
+                y = max(data[len(data) - 1]['y'] + _get_random(-1, 1), 0)
             data.append({
                 'x': j,
                 'y': y,
@@ -42,12 +42,12 @@ def generate_multi_time_series_data(n, m):
 def generate_scatter_plot_data(n):
     data = []
     seed(1)
-    a = get_random(-1, 1)
-    b = get_random(0, 1)
+    a = _get_random(-1, 1)
+    b = _get_random(0, 1)
 
     for i in range(n):
-        x = get_random(-1, 1)
-        y = x * a + get_random(0, b)
+        x = _get_random(-1, 1)
+        y = x * a + _get_random(0, b)
         data.append({'x': x, 'y': y})
 
     return data
@@ -58,11 +58,11 @@ def generate_multi_scatter_plot_data(n, m):
     seed(1)
     for i in range(m):
         group = 'group ' + i
-        a = get_random(-1, 1)
-        b = get_random(0, 1)
+        a = _get_random(-1, 1)
+        b = _get_random(0, 1)
         for j in range(n):
-            x = get_random(-1, 1)
-            y = x * a + get_random(0, b)
+            x = _get_random(-1, 1)
+            y = x * a + _get_random(0, b)
             data.append({
                 'x': x,
                 'y': y,
@@ -75,7 +75,7 @@ def generate_histogram_data(n):
     data = []
     seed(1)
     for i in range(n):
-        x = round(get_random(1, 10))
+        x = round(_get_random(1, 10))
         data.append({
             'x': x
         })
